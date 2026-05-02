bd19f6b84cf168396b144eaff12be26c5748ba8e
YeaeunnKim <kimyeaeun0611@gmail.com>
2026-05-02 17:00:14 +0900

    사과문 반영

---
+++ app/(tabs)/message.tsx
@@ -0,0 +1,5 @@
+import MessageScreen from '@/src/screens/MessageScreen';
+
+export default function MessageTabScreen() {
+  return <MessageScreen />;
+}
--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -16,6 +16,7 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'clock.fill': 'timeline',
+  'message.fill': 'chat',
   'person.fill': 'person',
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
